d7c5b08d3cbd40ff35e59abc8a918f524f46b049
fedorovmichael <mishania.f@gmail.com>
2017-04-20 09:33:36 +0300

    push sendSMS file

---
+++ public/javascripts/code/sendSMS.js
@@ -0,0 +1,27 @@
+
+function sendSMS()
+{
+    var id = $('#hdnSelectedList').val();
+    var data = {};
+    data.id = id;
+    sendDataToServer('/lists/getSelectedItemsAndPhones', data, sendSMSDataHandler, '');
+}
+
+function sendSMSDataHandler(response)
+{    
+    var selectedItems = response.selectedItems;
+    var phoneNumbers = response.phoneNumbers;    
+
+    $.each(phoneNumbers, function(indexPhone, phone){
+
+        var message = '';
+        var phoneNumber = phone.number;    
+        message += "שלום\n";
+
+        $.each(selectedItems, function(indexItem, item){
+            message += item.name +" - " + item.count + " \n";
+        });
+               
+        var result = window.app.sendSMS(phoneNumber, message);
+    });    
+}
